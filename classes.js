4cad0d2946e42c7fe9c9880d0ccf73fc207049df
Trevor hoffman <trevorhoffman@Trevors-MacBook-Air.local>
2019-03-22 15:59:40 -0500

    the end of the project

--- classes.js
+++ classes.js
@@ -144,6 +144,25 @@ fire(index) {
         - The anonymous function should decrease wear_and_tear_count by 10, and set needs_reboot to false
 */
 
-//Code Here
+class Machine{
+  constructor() {
+    this.widgets_made_count = 0;
+    this.wear_and_tear_count = 0;
+    this.needs_reboot = false;
+  }
+  makeWidgets(num) {
+    this.widgets_made_count += num;
+    this.wear_and_tear_count += Math.floor(num/50);
+  }
+  fixMachine() {
+    this.needs_reboot = true;
+  }
+  reboot() {
+    return function() {
+      this.wear_and_tear_count -= 10;
+      this.needs_reboot = false;
+    }.bind(this)
+  }
+}
 
 
